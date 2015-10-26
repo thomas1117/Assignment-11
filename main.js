201640bc9ed6525912a72c672700b08f55bd823f
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-10-25 22:51:15 -0700

    scrap

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 $(document).ready(function(){
 
+
+
+$("#signInButton").on("click",function(){
+	$("#signInForm").toggleClass("hidden");
+	$("#masterContainer").toggleClass("overlay");
+});
+
+$("#cat1").hover(function(){
+	$(".catDiv").addClass("displayBlock");
+});
+
+
+
+
+
+
+
+
+
+
+
+
 	var mapData = data.results.map(function(obj){
 		return {
 			'title': unescape(obj.title),
 			'price': obj.price,
 			'shop_name': obj.Shop.shop_name,
 			'currency_code':obj.currency_code,
-			'url_170x135': obj.Images[0]['url_170x135']
+			'url_170x135': obj.Images[0]['url_170x135'],
+			'url':obj.url
 		};
 	});
 
